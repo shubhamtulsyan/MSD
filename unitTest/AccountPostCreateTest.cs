df7009fac828f2e6749ba7cd78930d6b0577cae7
shubhamtulsyan <36358567+shubhamtulsyan@users.noreply.github.com>
2019-06-06 17:40:59 +0530

    commit 2

---
+++ unitTest/AccountPostCreateTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeItEasy;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using HSBC.AMG.Plugins;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace unitTest
+{
+    [TestClass]
+    public class AccountPostCreateTest
+    {
+        [TestMethod]
+        public void AccPostCreateTest()
+        {
+            var fakeContext = new XrmFakedContext();
+
+            Entity acc = new Entity("account");
+            acc.Id = Guid.NewGuid();
+            acc.Attributes.Add("name", "ABC Ltd");
+
+            var fakeService = fakeContext.GetOrganizationService();
+
+            fakeContext.ExecutePluginWithTarget<AccountPostCreate>(acc, "Create", 40);
+
+            //retrieve task record
+            QueryExpression query = new QueryExpression("task");
+            query.ColumnSet.AddColumn("subject");
+            
+            EntityCollection col = fakeService.RetrieveMultiple(query);
+
+            Assert.IsTrue(col.Entities.Count == 1);
+        }
+    }
+}
---
+++ unitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("unitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6a34c03-c880-449c-872c-514e258c487f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
